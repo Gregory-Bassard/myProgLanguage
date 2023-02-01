9bb249fcf15956eaa697d265e915c62918856fa5
cp-19grb <gregory.bassard@ceff.ch>
2023-02-01 16:26:08 +0100

    + fonction de base (LEDOn/LEDOff) + fonction click_btn_Run qui récupére chaque lignes et mots taper dans le TextBox + fonction Séquentiel (SET/RESET/WAIT) <!> problème sur le refresh de la MainWindow. Que le résultat final est afficher et les WAIT ne pérmette pas de voire les résultats intérmédiaire. (peut être un problème avec la fonction WAIT ou le fait d'être bloquer dans l'event click su btn Run) <!>

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -26,9 +28,92 @@ namespace myProgLanguage
             InitializeComponent();
 
         }
-        public void LedON(int ledId)
+
+
+
+        public void LedOn(int ledId)
         {
             Ellipse Led = (Ellipse)FindName($"LED{ledId}");
+            Led.Fill = Brushes.Green;
+            InvalidateVisual();
+        }
+        public void LedOff(int ledId)
+        {
+            Ellipse Led = (Ellipse)FindName($"LED{ledId}");
+            Led.Fill = Brushes.Red;
+        }
+        public void Restart()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                LedOff(i);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Restart();
+            
+            //Lire le code
+            string text = TextZone.Text;
+            string[] lines = text.Split("\n");
+            foreach (string line in lines)
+            {
+                string[] words = line.Split(" ");
+                switch (words[0])
+                {
+                    case "SET":
+                        SET(words);
+                        break;
+                    case "RESET":
+                        RESET(words);
+                        break;
+                    case "WAIT":
+                        WAIT(words);
+                        break;
+                    case "LET":
+                        break;
+                    case "INC":
+                        break;
+                }
+            }
+        }
+        public void SET(string[] words)
+        {
+            int param_x = 0;
+
+            //TODO : vérifier si words[1] n'est pas une variable ($___)
+
+            //vérifier si words[1] peut etre convertie en int
+            if (int.TryParse(words[1], out param_x))
+            {
+                LedOn(param_x);
+            }
+        }
+
+        public void RESET(string[] words)
+        {
+            int param_x = 0;
+
+            //TODO : vérifier si words[1] n'est pas une variable ($___)
+
+            //vérifier si words[1] peut etre convertie en int
+            if (int.TryParse(words[1], out param_x))
+            {
+                LedOff(param_x);
+            }
+        }
+        public void WAIT(string[] words)
+        {
+            int param_xxx = 0;
+
+            //TODO : vérifier si words[1] n'est pas une variable ($___)
+
+            //vérifier si words[1] peut etre convertie en int
+            if (int.TryParse(words[1], out param_xxx))
+            {
+                Thread.Sleep(param_xxx);
+            }
         }
     }
 }
