657d442a489cfdf06ab7a34734dac669ac044d9c
cp-19grb <gregory.bassard@ceff.ch>
2023-02-08 09:42:10 +0100

    + methode SearchVar de la classe Variable pour chercher une variable avec son nom modif de la methode LET (fini) + méthod INC + méthode RemoveBackslash pour enlever les \t\n\r et optionellement le $

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -75,6 +75,7 @@ namespace myProgLanguage
                         LET(words);
                         break;
                     case "INC":
+                        INC(words);
                         break;
                 }
             }
@@ -82,56 +83,99 @@ namespace myProgLanguage
         public void SET(string[] words)
         {
             int param_x = -1;
+
             //vérifier si words[1] est une variable ($___)
             if (words[1].Count() > 1 && words[1][0] == '$')
             {
-                variables.ForEach(var =>
-                {
-                    if (var.name == words[1].Remove(0, 1))
-                    {
-                        int.TryParse(var.val, out param_x);
-                        return;
-                    }
-                });
+                Variable var = Variable.SearchVar(variables, RemoveBackslash_t_n_r(words[1], true));
+                int.TryParse(var.val, out param_x);
             }
             //vérifier si words[1] peut etre convertie en int
             else
-                int.TryParse(words[1], out param_x);
+                int.TryParse(RemoveBackslash_t_n_r(words[1]), out param_x);
+
+            //TODO : Géstion des erreurs
             leds[param_x].On();
         }
         public void RESET(string[] words)
         {
             int param_x = 0;
-            //TODO : vérifier si words[1] n'est pas une variable ($___)
 
+            //vérifier si words[1] n'est pas une variable ($___)
+            if (words[1].Count() > 1 && words[1][0] == '$')
+            {
+                Variable var = Variable.SearchVar(variables, RemoveBackslash_t_n_r(words[1], true));
+                int.TryParse(var.val, out param_x);
+            }
             //vérifier si words[1] peut etre convertie en int
-            if (int.TryParse(words[1], out param_x))
-                leds[param_x].Off();
+            else
+                int.TryParse(RemoveBackslash_t_n_r(words[1]), out param_x);
+
+            //TODO : Géstion des erreurs
+            leds[param_x].Off();
         }
         public void WAIT(string[] words)
         {
             int param_xxx = 0;
-            //TODO : vérifier si words[1] n'est pas une variable ($___)
 
-            //vérifier si words[1] peut etre convertie en int
-            if (int.TryParse(words[1], out param_xxx))
-                Thread.Sleep(param_xxx);
-        }
-        public void LET(string[] words)
-        {
-            int param_val = 0;
-            Variable newVar = new Variable("name", 0);
-            //TODO : vérifier si words[1] n'est pas une variable ($___)
-
-            //vérifier si words[1] peut etre convertie en int
+            //vérifier si words[1] n'est pas une variable ($___)
             if (words[1].Count() > 1 && words[1][0] == '$')
             {
-                if (int.TryParse(words[2], out param_val))
-                    newVar = new Variable(words[1].Remove(0, 1), param_val);
+                Variable var = Variable.SearchVar(variables, RemoveBackslash_t_n_r(words[1], true));
+                int.TryParse(var.val, out param_xxx);
+            }
+            //vérifier si words[1] peut etre convertie en int
+            else
+                int.TryParse(RemoveBackslash_t_n_r(words[1]), out param_xxx);
+
+            //TODO : Géstion des erreurs
+            Thread.Sleep(param_xxx);
+        }
+        public void LET(string[] words) //TODO: géréer l'ajout d'une variable qui éxiste déja
+        {
+            int param_val = 0;
+            Variable newVar = null;
+            
+            //vérifier si words[1] est bien une variable ($___)
+            if (words[1].Count() > 1 && words[1][0] == '$')
+            {
+                //vérifier si words[2] n'est pas une variable ($___)
+                if (words[2].Count() > 1 && words[2][0] == '$')
+                {
+                    Variable var = Variable.SearchVar(variables, RemoveBackslash_t_n_r(words[2], true));
+                    int.TryParse(var.val, out param_val);
+                }
                 else
-                    newVar = new Variable(words[1].Remove(0, 1), words[2], "STRING");
+                    int.TryParse(RemoveBackslash_t_n_r(words[2]), out param_val);
+                newVar = new Variable(variables.Count(), RemoveBackslash_t_n_r(words[1], true), param_val);
                 variables.Add(newVar);
             }
+        }
+        public void INC(string[] words)
+        {
+            int param_val = 0;
+
+            //vérifier si words[1] est bien une variable ($___)
+            if (words[1].Count() > 1 && words[1][0] == '$')
+            {
+                int varId = Variable.SearchVar(variables, RemoveBackslash_t_n_r(words[1], true)).id;
+
+                //vérifier si words[2] peut etre convertie en int
+                if (int.TryParse(RemoveBackslash_t_n_r(words[2]), out param_val))
+                {
+                    variables[varId].val = (int.Parse(variables[varId].val) + param_val).ToString();
+                }                
+            }
+        }
+        public string RemoveBackslash_t_n_r(string s, bool dollar = false)
+        {
+            string newS = s.Replace("\r", string.Empty);
+            newS = newS.Replace("\n", string.Empty);
+            newS = newS.Replace("\t", string.Empty);
+            if (dollar)
+                newS = newS.Replace("$", string.Empty);
+
+            return newS;
         }
     }
 }
--- Variable.cs
+++ Variable.cs
@@ -3,26 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Documents;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace myProgLanguage
 {
     internal class Variable
     {
+        public int id { get; set; }
         public string name {get; set;}
         public string type { get; set; }
         public string val { get; set; }
 
-        public Variable(string name, string val, string type = "STRING")
+        public Variable(int id, string name, string val, string type = "STRING")
         {
+            this.id = id;
             this.name = name;
             this.type = type;
             this.val = val;
         }
-        public Variable(string name, int val, string type = "INT")
+        public Variable(int id, string name, int val, string type = "INT")
         {
+            this.id = id;
             this.name = name;
             this.type = type;
             this.val = val.ToString();
+        }
+        public static Variable SearchVar(List<Variable> vars ,string varName)
+        {
+            Variable var = null;
+
+            vars.ForEach(v =>
+            {
+                if (v.name == varName)
+                {
+                    var = v;
+                }
+            });
+            return var;
         }
     }
 }
